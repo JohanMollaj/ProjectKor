d5e72928b64558f967b20a7b03c5513faca6eb4b
Johan Mollaj <johanmollaj12@gmail.com>
2025-01-27 22:16:16 +0100

    added sign in and sign up pages. added mobile compatibility

--- server.py
+++ server.py
@@ -13,6 +13,9 @@ templates = Jinja2Templates(directory="static")
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse(name="index.html", request=request)
+@app.get("/signin")
+async def root(request: Request):
+    return templates.TemplateResponse(name="signin.html", request=request)
 
 @app.post("/login")
 async def login(request: Request, username: str = Form(...), password: str = Form(...)):
